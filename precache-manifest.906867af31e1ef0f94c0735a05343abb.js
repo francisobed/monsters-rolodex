1cf00870a8849829417449257ecf48cf7bf37afb
francisobed <francisobadiahajigana@gmail.com>
2020-09-15 12:00:30 +0100

    Updates

---
+++ precache-manifest.906867af31e1ef0f94c0735a05343abb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16b8ce1a194c06dca3f0206fce529366",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e5c671d6ddf73b67ab64",
+    "url": "/monsters-rolodex/static/css/main.47c794e5.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monsters-rolodex/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5c671d6ddf73b67ab64",
+    "url": "/monsters-rolodex/static/js/main.dbaca7ea.chunk.js"
+  },
+  {
+    "revision": "f553da120d846b640e5e",
+    "url": "/monsters-rolodex/static/js/runtime-main.1dfd2aab.js"
+  }
+]);
